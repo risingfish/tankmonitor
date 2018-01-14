3bdedd4eacdd47906dd9ba8bc46877e761908833
Zac Feuerborn <risingfish@gmail.com>
2018-01-13 23:26:48 -0700

    More updates

--- src/Email.js
+++ src/Email.js
@@ -1,3 +1,5 @@
+var nodemailer = require('nodemailer');
+
 /**
  *
  * @constructor
@@ -10,11 +12,33 @@ var Email = function() {
     };
 
     this.sendEmail = function (address, message, carrier) {
+        var transporter = nodemailer.createTransport({
+            host: 'homestor',
+            port: 25,
+            secure: false // true for 465, false for other ports
+        });
 
+        var mailOptions = {
+            from: '"Zac" <risingfish@gmail.com>', // sender address
+            to: '2087249509@vtext.com, 2082837091@vtext.com', // list of receivers
+            subject: '', // Subject line
+            text: 'The quarantine tank is low! Please check the water levels!' // plain text body
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+            // Preview only available when sending through an Ethereal account
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        });
     };
 };
 
 Email.CARRIER_VERIZON = 'vtext.com';
+Email.CARRIER_ATT = 'txt.att.net';
 
 module.exports = Email;
 
